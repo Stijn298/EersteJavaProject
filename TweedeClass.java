ded3f594e70de8b586b02a48a268949bbeb2e3a3
Stijn K <sdekoning2@gmail.com>
2023-05-15 12:17:24 +0200

    De TweedeClass heeft nu inoud

--- TweedeClass.java
+++ TweedeClass.java
@@ -1,2 +1,10 @@
+/*
+Ik heb hier nu dingen gedaan, maar weet niet precies hoe ik ze gebruik.
+*/
 public class TweedeClass {
+    public static void main(String[] args) {
+        beamer = "Dat **** ding"
+        System.out.println("De beamer ging net uit");
+        System.out.println("TweedeClass.main");
+    }
 }
