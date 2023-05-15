d428d069bc5ff26de7d61b3536a6da861978df99
Stijn K <sdekoning2@gmail.com>
2023-05-15 11:44:42 +0200

    Tekst aangepast
    De tekst die geprint word is een klein beetje aangepast

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Gegroet");
         System.out.print("Ik ben Stijn\n");
-        System.out.print("En ik heb wat geschreven.\nDit heb ik op twee manieren gedaan.\nKnap he.");
+        System.out.print("En ik heb wat geschreven.\nDit heb ik op verschillende manieren gedaan.\nKnap he.");
     }
 }
 /*
