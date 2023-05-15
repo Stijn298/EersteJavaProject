fe51e99095ab1fe4eabffc8e33fb8074e5318e1f
Stijn K <sdekoning2@gmail.com>
2023-05-15 11:37:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Zou de code nog werken met comments er in? Vraag ik me af!
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gegroet");
+        System.out.print("Ik ben Stijn\n");
+        System.out.print("En ik heb wat geschreven.\nDit heb ik op twee manieren gedaan.\nKnap he.");
+    }
+}
+/*
+Dit is een comment. Blijkbaar is mijn standaard code anders dan die van anderen
+met ctrl / kun je automatisch 2 van deze / neer zetten. Handig voor een single line comment!
+ */
