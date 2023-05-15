249ad248912ca3ac2fa090eef4969c8f01c55393
Stijn K <sdekoning2@gmail.com>
2023-05-15 12:35:26 +0200

    Inhoud Main verplaats naar IntroductieClass. TweedeClass verwijdert

---
+++ src/IntroductieClass.java
@@ -0,0 +1,9 @@
+public class IntroductieClass {
+    public IntroductieClass() {
+        System.out.println("Gegroet");
+        System.out.print("Ik ben Stijn\n");
+        System.out.print("En ik heb wat geschreven.\nDit heb ik op verschillende manieren gedaan.\nKnap he.");
+//        System.out.print("En ik heb wat geschreven.\nDit heb ik op verschillende manieren gedaan.\nKnap he.");
+//        Met ctrl d copieer je een lijn}
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 // Zou de code nog werken met comments er in? Vraag ik me af!
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gegroet");
-        System.out.print("Ik ben Stijn\n");
-        System.out.print("En ik heb wat geschreven.\nDit heb ik op verschillende manieren gedaan.\nKnap he.");
+        IntroductieClass tijdelijk = new IntroductieClass();
     }
+
 }
 /*
 Dit is een comment. Blijkbaar is mijn standaard code anders dan die van anderen
